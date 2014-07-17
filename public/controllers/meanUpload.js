e5ec651a31f8faa2a2562df16c015acccb5ec33c
Orit Persik <orit@linnovate.net>
2014-07-17 17:27:11 +0300

    initial commit

---
+++ public/controllers/meanUpload.js
@@ -0,0 +1,20 @@
+'use strict';
+
+angular.module('mean.mean-upload').controller('MeanUploadController', ['$scope', 'Global', 'MeanUpload',
+    function($scope, Global, MeanUpload) {
+        $scope.global = Global;
+        $scope.package = {
+            name: 'mean-upload'
+        };
+
+        $scope.images = [];
+
+        $scope.uploadFileCallback = function(file) {
+            console.log(file);
+        };
+
+        $scope.uploadFinished = function(files) {
+            console.log(files);
+        };
+    }
+]);
